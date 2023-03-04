16fd89dc4a92ef7815fb0d88620b44f523bbca56
Sugafar <bobbeasley@gmail.com>
2023-03-04 09:12:52 -0600

    Initial Commit

---
+++ TechAssistV2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechAssistV2
+//
+//  Created by Raf on 3/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
