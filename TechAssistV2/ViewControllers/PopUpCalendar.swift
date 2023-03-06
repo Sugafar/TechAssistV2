1791409dca1f956bcac613854547ea6396df2749
Sugafar <bobbeasley@gmail.com>
2023-03-06 11:23:08 -0600

    date and time pickers are working . Needs formatting

--- TechAssistV2/ViewControllers/PopUpCalendar.swift
+++ TechAssistV2/ViewControllers/PopUpCalendar.swift
@@ -1,32 +1,40 @@
 //
-//  PopUpWindow.swift
+//  PopUpCalendar.swift
 //  TechAssistV2
 //
-//  Created by Raf on 3/5/23.
+//  Created by Raf on 3/6/23.
 //
 
 import Foundation
 import UIKit
 
-public class PopUpWindow: UIViewController {
+public class PopUpCalendar: UIViewController {
 
-    public let popUpWindowView = PopUpWindowView()
+    public let popUpCalendarView = PopUpCalendarView()
     
     init(title: String, text: String, buttontext: String) {
         super.init(nibName: nil, bundle: nil)
         modalTransitionStyle = .crossDissolve
         modalPresentationStyle = .overFullScreen
         
-        popUpWindowView.popupTitle.text = title
-        popUpWindowView.popupText.text = text
-        popUpWindowView.popupButton.setTitle(buttontext, for: .normal)
-        popUpWindowView.popupButton.addTarget(self, action: #selector(dismissView), for: .touchUpInside)
-        view = popUpWindowView
+        popUpCalendarView.popupTitle.text = title
+       // popUpCalendarView.popupText.text = text
+        popUpCalendarView.popupButton.setTitle(buttontext, for: .normal)
+        popUpCalendarView.popupButton.addTarget(self, action: #selector(dismissView), for: .touchUpInside)
+        view = popUpCalendarView
+        
+        popUpCalendarView.datePicker.addTarget(self, action: #selector(datePickerValueChanged(_:)), for: .valueChanged)
+        
     }
 
     required init(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    @objc func datePickerValueChanged(_ sender: UIDatePicker) {
+            // Handle value changes here
+            print("Selected date: \(sender.date)")
+        }
     
     
     @objc func dismissView(){
@@ -35,23 +43,25 @@ public class PopUpWindow: UIViewController {
 
 }
 
-public class PopUpWindowView: UIView {
+public class PopUpCalendarView: UIView {
     
     let popupView = UIView(frame: CGRect.zero)
     public var popupTitle = UILabel(frame: CGRect.zero)
-    let popupText = UILabel(frame: CGRect.zero)
+   // let popupText = UILabel(frame: CGRect.zero)
     let popupButton = UIButton(frame: CGRect.zero)
     
     let BorderWidth: CGFloat = 2.0
+    
+    var datePicker: UIDatePicker!
     
     init() {
         super.init(frame: CGRect.zero)
         // Semi-transparent background
         backgroundColor = UIColor.black.withAlphaComponent(0.3)
-       
+       datePicker = UIDatePicker()
         // Popup Background
         //popupView.backgroundColor = UIColor.colorFromHex("#BC214B")
-        popupView.backgroundColor = UIColor.colorFromHex("#198754").withAlphaComponent(0.8)
+        popupView.backgroundColor = UIColor.colorFromHex("#198754").withAlphaComponent(0.7)
         popupView.layer.borderWidth = BorderWidth
         popupView.layer.masksToBounds = true
         popupView.layer.borderColor = UIColor.white.cgColor
@@ -69,20 +79,35 @@ public class PopUpWindowView: UIView {
         popupTitle.textAlignment = .center
         
         // Popup Text
-        popupText.textColor = UIColor.white
-        popupText.font = UIFont.systemFont(ofSize: 16.0, weight: .semibold)
-        popupText.numberOfLines = 0
-        popupText.textAlignment = .left
+//        popupText.textColor = UIColor.white
+//        popupText.font = UIFont.systemFont(ofSize: 16.0, weight: .semibold)
+//        popupText.numberOfLines = 0
+//        popupText.textAlignment = .left
         
         // Popup Button
         popupButton.setTitleColor(UIColor.white, for: .normal)
         popupButton.titleLabel?.font = UIFont.systemFont(ofSize: 23.0, weight: .bold)
        // popupButton.backgroundColor = UIColor.colorFromHex("#9E1C40")
         popupButton.backgroundColor = UIColor.colorFromHex("#198754")
+        // Set the mode of the date picker to display both date and time
+        popupView.addSubview(datePicker)
+        
+              datePicker.datePickerMode = .dateAndTime
+
+              // Set the minimum date to today's date
+              datePicker.minimumDate = Calendar.current.date(byAdding: .day, value: -7, to: Date())
+
+              // Set the maximum date to one year from today's date
+              datePicker.maximumDate = Calendar.current.date(byAdding: .day, value: 7, to: Date())
+
+              // Add a target action to the date picker to handle value changes
+      //  datePicker.addTarget(self, action: #selector(popupCalendar.datePickerValueChanged(_:)), for: .valueChanged)
+        
         
         popupView.addSubview(popupTitle)
-        popupView.addSubview(popupText)
+       // popupView.addSubview(popupText)
         popupView.addSubview(popupButton)
+       
         
         // Add the popupView(box) in the PopUpWindowView (semi-transparent background)
         addSubview(popupView)
@@ -106,14 +131,14 @@ public class PopUpWindowView: UIView {
             ])
         
         
-        // PopupText constraints
-        popupText.translatesAutoresizingMaskIntoConstraints = false
+        // datePicker constraints
+        datePicker.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            popupText.heightAnchor.constraint(greaterThanOrEqualToConstant: 67),
-            popupText.topAnchor.constraint(equalTo: popupTitle.bottomAnchor, constant: 8),
-            popupText.leadingAnchor.constraint(equalTo: popupView.leadingAnchor, constant: 15),
-            popupText.trailingAnchor.constraint(equalTo: popupView.trailingAnchor, constant: -15),
-            popupText.bottomAnchor.constraint(equalTo: popupButton.topAnchor, constant: -8)
+            datePicker.heightAnchor.constraint(greaterThanOrEqualToConstant: 67),
+            datePicker.topAnchor.constraint(equalTo: popupTitle.bottomAnchor, constant: 8),
+            datePicker.leadingAnchor.constraint(equalTo: popupView.leadingAnchor, constant: 15),
+            datePicker.trailingAnchor.constraint(equalTo: popupView.trailingAnchor, constant: -15),
+            datePicker.bottomAnchor.constraint(equalTo: popupButton.topAnchor, constant: -8)
             ])
 
         
@@ -126,6 +151,8 @@ public class PopUpWindowView: UIView {
             popupButton.bottomAnchor.constraint(equalTo: popupView.bottomAnchor, constant: -BorderWidth)
             ])
         
+      
+        
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -134,27 +161,28 @@ public class PopUpWindowView: UIView {
     
 }
 
-extension UIColor {
-    static func rgbColor(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
-        return UIColor.init(red: red/255, green: green/255, blue: blue/255, alpha: 1.0)
-    }
+//extension UIColor {
+//    static func rgbColor(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+//        return UIColor.init(red: red/255, green: green/255, blue: blue/255, alpha: 1.0)
+//    }
+//
+//    static func colorFromHex(_ hex: String) -> UIColor {
+//        var hexString = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+//        if hexString.hasPrefix("#") {
+//            hexString.remove(at: hexString.startIndex)
+//        }
+//        if hexString.count != 6 {
+//            return UIColor.magenta
+//        }
+//
+//        var rgb: UInt64 = 0
+//        Scanner.init(string: hexString).scanHexInt64(&rgb)
+//
+//        return UIColor.init(red: CGFloat((rgb & 0xFF0000) >> 16)/255,
+//                            green: CGFloat((rgb & 0x00FF00) >> 8)/255,
+//                            blue: CGFloat(rgb & 0x0000FF)/255,
+//                            alpha: 1.0)
+//    }
     
-    static func colorFromHex(_ hex: String) -> UIColor {
-        var hexString = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
-        if hexString.hasPrefix("#") {
-            hexString.remove(at: hexString.startIndex)
-        }
-        if hexString.count != 6 {
-            return UIColor.magenta
-        }
-        
-        var rgb: UInt64 = 0
-        Scanner.init(string: hexString).scanHexInt64(&rgb)
-        
-        return UIColor.init(red: CGFloat((rgb & 0xFF0000) >> 16)/255,
-                            green: CGFloat((rgb & 0x00FF00) >> 8)/255,
-                            blue: CGFloat(rgb & 0x0000FF)/255,
-                            alpha: 1.0)
-    }
-    
-}
+//}
+
--- TechAssistV2/ViewControllers/TimeEntry.swift
+++ TechAssistV2/ViewControllers/TimeEntry.swift
@@ -7,10 +7,20 @@
 
 import UIKit
 
-class TimeEntry:UIViewController{
+class TimeEntry:UIViewController,UIPickerViewDelegate, UIPickerViewDataSource{
     
     let btnTimePU = UIButton()
+    let btnDate = UIButton()
     var popUpWindow: PopUpWindow!
+   // var popUpCalendar: PopUpCalendar!
+    var pickerView: UIPickerView!
+    var datePicker: UIDatePicker!
+
+        let column1Options = ["1", "2", "3","4","5","6","7","8","9","10","11","12","13","14"]
+    let column2Options = [":00",":15", ":30", ":45"]
+    let column1Heading = "Hour"
+        let column2Heading = "Min"
+    let columnHeadingFont = UIFont.boldSystemFont(ofSize: 12)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,16 +29,122 @@ class TimeEntry:UIViewController{
         self.navigationController?.navigationBar.tintColor = UIColor.blue
         title = "Time Entry"
         
+        datePicker = UIDatePicker()
+        pickerView = UIPickerView()
+        // Set the mode of the date picker to display both date and time
+                datePicker.datePickerMode = .dateAndTime
+
+                // Set the minimum date to today's date
+                datePicker.minimumDate = Date()
+
+                // Set the maximum date to one year from today's date
+                datePicker.maximumDate = Calendar.current.date(byAdding: .year, value: 1, to: Date())
+
+                // Add a target action to the date picker to handle value changes
+                datePicker.addTarget(self, action: #selector(datePickerValueChanged(_:)), for: .valueChanged)
+           
+        datePicker.datePickerMode = .date
+        datePicker.preferredDatePickerStyle = .compact
+        
         setupUI()
         
     }
     
+    @objc func datePickerValueChanged(_ sender: UIDatePicker) {
+        // Handle value changes here
+        print("Selected date: \(sender.date)")
+        // kludgy code to get the calendar to dismiss
+        self.datePicker.preferredDatePickerStyle = .wheels
+        self.datePicker.preferredDatePickerStyle = .automatic
+        
+    }
+    
+    //Begin the picker
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+            return 2
+        }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+            if component == 0 {
+                return column1Options.count
+            } else {
+                return column2Options.count
+            }
+        }
+    
+    func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
+           // Return the desired width of each column
+           return 55.0
+       }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        //            if component == 0 {
+        //                return column1Options[row]
+        //            } else {
+        //                return column2Options[row]
+        //            }
+        
+        if component == 0 {
+            if row == 0 {
+                return column1Heading
+            } else {
+                return column1Options[row - 1]
+            }
+        } else {
+            if row == 0 {
+                return column2Heading
+            } else {
+                return column2Options[row - 1]
+            }
+            
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+           var label: UILabel
+           if let view = view as? UILabel {
+               label = view
+           } else {
+               label = UILabel()
+           }
+           if row == 0 {
+               if component == 0 {
+                   label.text = column1Heading
+               } else {
+                   label.text = column2Heading
+               }
+               label.font = columnHeadingFont
+           } else {
+               if component == 0 {
+                   label.text = column1Options[row]
+               } else {
+                   label.text = column2Options[row]
+               }
+           }
+           return label
+       }
+   
+
+    //End the picker
+    
    @objc func popUpButtonAction(_ sender: UIButton) {
         
-        popUpWindow = PopUpWindow(title: "Error", text: "Sorry, that email address is already used!", buttontext: "OK")
+        popUpWindow = PopUpWindow(title: "Time Entry Help", text: "Time Entry Help", buttontext: "OK")
         self.present(popUpWindow, animated: true, completion: nil)
-       popUpWindow.popUpWindowView.popupTitle.text = "Hello from down here"
+       popUpWindow.popUpWindowView.popupText.text = "1. Pick a date by selecting today's date to open a calendar. If you're entering time for today skip this step \n\n2. Pick an hour and a minute \n\n3. Save it \n\nA day might have 4 entries: "
+       popUpWindow.popUpWindowView.popupText.text! += "\n\nWork started: 7:00, Ended: 11:00 \nWork started 12:00, Ended 16:00 \n"
+        
     }
+    
+//    @objc func popUpCalendarButtonAction(_ sender: UIButton) {
+//
+//        popUpCalendar = PopUpCalendar(title: "Pick a Date",text:"wtf", buttontext: "OK")
+//         self.present(popUpCalendar, animated: true, completion: nil)
+//       // popUpWindow.popUpWindowView.popupText.text = "1. Pick a date \n2. Pick an hour and a minute \n3. Save it \nA day might have 4 entries: "
+//       // popUpWindow.popUpWindowView.popupText.text! += "\nWork started: 7:00, Ended: 11:00 \nWork started 12:00, Ended 16:00"
+//
+//     }
+    
     
     func setupUI(){
         
@@ -78,25 +194,24 @@ class TimeEntry:UIViewController{
             
         ])
         
-        
         //Begin Time Btn
         
         
         view.addSubview(btnTimePU)
         
         btnTimePU.configuration = .tinted()
-        btnTimePU.configuration?.image = UIImage(systemName: "clock.fill")
+        btnTimePU.configuration?.image = UIImage(systemName: "questionmark.circle.fill")
         btnTimePU.configuration?.imagePadding = 6
         btnTimePU.configuration?.imagePlacement = .leading
         
         btnTimePU.titleLabel?.textColor = UIColor.black
-        btnTimePU.setTitle("Enter Time", for: .normal)
+       // btnTimePU.setTitle("Help", for: .normal)
         
         
         btnTimePU.translatesAutoresizingMaskIntoConstraints = false
-        btnTimePU.centerXAnchor.constraint(equalTo:view.centerXAnchor).isActive = true
-        btnTimePU.topAnchor.constraint(equalTo:imageView.bottomAnchor,constant: 45).isActive = true
-        btnTimePU.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        btnTimePU.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor).isActive = true
+        btnTimePU.topAnchor.constraint(equalTo:imageView.bottomAnchor,constant: 10).isActive = true
+        btnTimePU.widthAnchor.constraint(equalToConstant: 50).isActive = true
         btnTimePU.heightAnchor.constraint(equalToConstant: 50).isActive = true
         btnTimePU.addTarget(self,action: #selector(popUpButtonAction),for:.touchUpInside)
         btnTimePU.layer.cornerRadius = 5
@@ -104,6 +219,7 @@ class TimeEntry:UIViewController{
         // btnTimePU.setTitleColor(UIColor.white, for: .normal)
         btnTimePU.setTitleColor(UIColor.systemBlue, for: .selected)
         btnTimePU.isEnabled = true
+        btnTimePU.layer.zPosition = 1
         
         _ = UIButton(type: .system)
         if let hs1Color = UIColor(hex: "#198754") {
@@ -113,6 +229,44 @@ class TimeEntry:UIViewController{
         
         //End time Button
         
+        //Begin Date Btn
+              
+              
+        // datePicker constraints
+        view.addSubview(datePicker)
+        
+        datePicker.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            datePicker.heightAnchor.constraint(greaterThanOrEqualToConstant: 67),
+            datePicker.topAnchor.constraint(equalTo: btnTimePU.bottomAnchor, constant: 1),
+            datePicker.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+           // datePicker.trailingAnchor.constraint(equalTo: popupView.trailingAnchor, constant: -15),
+            //datePicker.bottomAnchor.constraint(equalTo: popupButton.topAnchor, constant: -8)
+            ])
+
+        
+        
+       
+        
+        
+        view.addSubview(pickerView)
+        pickerView.delegate = self
+        pickerView.dataSource = self
+      //  pickerView.frame = CGRect(x: 0, y: 0, width: view.bounds.width, height: pickerView.bounds.height)
+
+        pickerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            pickerView.leadingAnchor.constraint(equalTo: btnTimePU.trailingAnchor,constant: 75),
+            pickerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,constant: 35),
+            pickerView.widthAnchor.constraint(equalToConstant: 110),
+            pickerView.heightAnchor.constraint(equalToConstant: 150),
+             //contentInset = UIEdgeInsets(top: 10, left: 0, bottom: 10, right: 0)
+
+        ])
+        pickerView.layer.zPosition = 0
+        
+       
         
     }
 }
