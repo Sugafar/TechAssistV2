357ac762afe0b6ed6158b9f9bb559dae73bc3ba2
Sugafar <bobbeasley@gmail.com>
2023-03-08 08:41:19 -0600

    date picking logic is working

--- TechAssistV2/ViewControllers/TimeEntry.swift
+++ TechAssistV2/ViewControllers/TimeEntry.swift
@@ -21,12 +21,15 @@ class TimeEntry:UIViewController,UIPickerViewDelegate, UIPickerViewDataSource{
     var pickerView: UIPickerView!
     var datePicker: UIDatePicker!
     var netScreenWidth: CGFloat = 0.0
+    var lastDateTimeEntered = Date()
+    
+    
     //var mySubView: UIView!
-
-        let column1Options = ["","1", "2", "3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24"]
-    let column2Options = [" ",":00",":15", ":30", ":45"]
+    
+    var column1Options = ["","1", "2", "3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24"]
+    var column2Options = [" ",":00",":15", ":30", ":45"]
     let column1Heading = "Hour"
-        let column2Heading = "Min"
+    let column2Heading = "Min"
     let columnHeadingFont = UIFont.boldSystemFont(ofSize: 18)
     
     override func viewDidLoad() {
@@ -43,19 +46,21 @@ class TimeEntry:UIViewController,UIPickerViewDelegate, UIPickerViewDataSource{
         //subtract out the width of the 2 buttons and the picker to leave netScreenWidth for spacing ie: 50 + 50 + 140
         netScreenWidth = netScreenWidth - 240
         
+        lastDateTimeEntered = Calendar.current.date(byAdding: .day, value: -8, to: Date())!
+        print(lastDateTimeEntered)
       
         lblMyDay.text = ""
         
         datePicker = UIDatePicker()
         pickerView = UIPickerView()
         // Set the mode of the date picker to display both date and time
-                datePicker.datePickerMode = .dateAndTime
+                datePicker.datePickerMode = .date
 
                 // Set the minimum date to today's date
-                datePicker.minimumDate = Date()
+                datePicker.minimumDate = Calendar.current.date(byAdding: .day, value: -7, to: Date())
 
                 // Set the maximum date to one year from today's date
-                datePicker.maximumDate = Calendar.current.date(byAdding: .year, value: 1, to: Date())
+                datePicker.maximumDate = Calendar.current.date(byAdding: .day, value: 7, to: Date())
 
                 // Add a target action to the date picker to handle value changes
                 datePicker.addTarget(self, action: #selector(datePickerValueChanged(_:)), for: .valueChanged)
@@ -63,7 +68,16 @@ class TimeEntry:UIViewController,UIPickerViewDelegate, UIPickerViewDataSource{
         datePicker.datePickerMode = .date
         datePicker.preferredDatePickerStyle = .compact
         
+        setBeginningDate()
         setupUI()
+        
+    }
+    
+    //set the beginning date to 8 days previous as user can only go back 7 days and the beginning date has to be less than a date they can pick
+    // for the logic to work. This func is needed for reset as well as startup
+    func setBeginningDate(){
+        
+        lastDateTimeEntered = Calendar.current.date(byAdding: .day, value: -8, to: Date())!
         
     }
     
@@ -76,7 +90,7 @@ class TimeEntry:UIViewController,UIPickerViewDelegate, UIPickerViewDataSource{
        
         
     }
-    
+    //pulls the date component only out of a datetime
     func justDate (aDate:Date)->String{
         
         let dateformatter = DateFormatter()
@@ -105,12 +119,7 @@ class TimeEntry:UIViewController,UIPickerViewDelegate, UIPickerViewDataSource{
        }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        //            if component == 0 {
-        //                return column1Options[row]
-        //            } else {
-        //                return column2Options[row]
-        //            }
-        
+       
         if component == 0 {
             if row == 0 {
                 return column1Heading
@@ -150,29 +159,132 @@ class TimeEntry:UIViewController,UIPickerViewDelegate, UIPickerViewDataSource{
            }
            return label
        }
+    
+    func pickerView(_ pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
+           let title = "Row \(row)"
+           let attributedTitle = NSAttributedString(string: title, attributes: [.font: UIFont.systemFont(ofSize: 16)])
+           return attributedTitle
+       }
    
 
     //End the picker
     
     @objc func getTime(){
+        //pulls the selected value out of each columns array
+        let myLastHour = column1Options[pickerView.selectedRow(inComponent: 0)]
         
         var myStringTime:String = column1Options[pickerView.selectedRow(inComponent: 0)] + column2Options[pickerView.selectedRow(inComponent:1 )]
-        
-        if(lastEntry == "Init" || lastEntry == "End")
+   
+        print("Current selected hour: \(column1Options[pickerView.selectedRow(inComponent: 0)])")
+        print("Current selected min: \(column2Options[pickerView.selectedRow(inComponent: 1)])")
+        if(!verifyTime(newHour: column1Options[pickerView.selectedRow(inComponent: 0)], newMin: column2Options[pickerView.selectedRow(inComponent: 1)]))
         {
-            if(lblMyDay.text == "")
-            {
-                lblMyDay.text! = "Start: " + myStringTime
-            }
-            else{
-                lblMyDay.text! += "Start: " + myStringTime
-            }
-            lastEntry = "Start"
+            
+                let alertController = UIAlertController(title: "kabloooey", message: "Entry must be equal to or greater than the last entry.", preferredStyle: .alert)
+                    let okAction = UIAlertAction(title: "OK", style: .default) { (action:UIAlertAction!) in
+                        print("OK button tapped");
+                    }
+                //alertController.view.backgroundColor = UIColor.gray.withAlphaComponent(0.2)
+                    alertController.addAction(okAction)
+                    self.present(alertController, animated: true, completion:nil)
+                
+            
         }
         else{
-            lblMyDay.text! += " End: " + myStringTime + "\n"
-            lastEntry = "End"
+            if(lastEntry == "Init" || lastEntry == "End")
+            {
+                if(lblMyDay.text == "")
+                {
+                    lblMyDay.text! = "Start: " + myStringTime
+                }
+                else{
+                    lblMyDay.text! += "Start: " + myStringTime
+                }
+                lastEntry = "Start"
+            }
+            else{
+                lblMyDay.text! += " End: " + myStringTime + "\n"
+                lastEntry = "End"
+            }
+            
+            hourArrayHandler(lastHour: (myLastHour))
         }
+            
+        
+        
+        
+    }
+    
+    func verifyTime(newHour:String,newMin:String) ->Bool{
+        
+        //example
+        
+        //let myDay = "3/7/2023"
+        
+        let myHour = newHour
+        let myMinute = newMin
+        var eDate = Date()
+
+        let dateFormatter2 = DateFormatter()
+        dateFormatter2.dateFormat = "MM/dd/yyyy"
+        let myDay = (dateFormatter2.string(from:datePicker.date))
+        let dateString = "\(myDay) \(myHour)\(myMinute)"
+        print("elon's date: \(dateString)")
+        dateFormatter2.dateFormat = "MM/dd/yyyy hh:mm"
+        eDate = dateFormatter2.date(from: dateString)! //{
+           // dateFormatter2.dateFormat = "MM/dd/yyyy hh:mm"
+            let formattedDateString = dateFormatter2.string(from: eDate)
+            print(formattedDateString)
+//        } else {
+//            print("Invalid date format")
+//        }
+        
+        //end of example
+        
+        // clean out the : from the last min
+        let myLastMin = String(newMin.filter { !":".contains($0) })
+        
+        //veify that the last time entered is less than the current entry
+        print("last Time: \(lastDateTimeEntered)")
+        print("new Time: \(eDate)")
+        if(lastDateTimeEntered <= eDate )// it's a valid entry as it's equal to or greater than the last time entered
+        {
+            lastDateTimeEntered = eDate // lastDateTimeEntered becomes the current entry
+            print("lastDateTimeEntered after update: \(lastDateTimeEntered)")
+            return true
+
+        }
+        else
+        {
+            // popup an alert that entry is not valid
+            return false
+        }
+        
+       
+      
+    }
+    
+    func hourArrayHandler(lastHour:String){
+        
+        column1Options.removeAll()
+        var hourNum:UInt8 = UInt8(lastHour)!
+        //var stringNum = String()
+        //var currentHour:Int = hourNum
+        column1Options.append(String("")) // this is to handle the column header
+        while hourNum < 25{
+           // stringNum = String(hourNum)
+            column1Options.append(String(hourNum))
+            hourNum += 1
+            
+        }
+        pickerView.reloadComponent(0)
+        let componentCount = pickerView.numberOfComponents
+
+        // Loop through each component and select the first row (index 0)
+        for component in 0..<componentCount {
+            pickerView.selectRow(0, inComponent: component, animated: true)
+        }
+        
         
     }
     
@@ -185,14 +297,7 @@ class TimeEntry:UIViewController,UIPickerViewDelegate, UIPickerViewDataSource{
         
     }
     
-//    @objc func popUpCalendarButtonAction(_ sender: UIButton) {
-//
-//        popUpCalendar = PopUpCalendar(title: "Pick a Date",text:"wtf", buttontext: "OK")
-//         self.present(popUpCalendar, animated: true, completion: nil)
-//       // popUpWindow.popUpWindowView.popupText.text = "1. Pick a date \n2. Pick an hour and a minute \n3. Save it \nA day might have 4 entries: "
-//       // popUpWindow.popUpWindowView.popupText.text! += "\nWork started: 7:00, Ended: 11:00 \nWork started 12:00, Ended 16:00"
-//
-//     }
+
     
     
     func setupUI(){
@@ -328,7 +433,7 @@ class TimeEntry:UIViewController,UIPickerViewDelegate, UIPickerViewDataSource{
         NSLayoutConstraint.activate([
             pickerView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             pickerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,constant: 90),
-            pickerView.widthAnchor.constraint(equalToConstant: 140),
+            pickerView.widthAnchor.constraint(equalToConstant: 170),
             pickerView.heightAnchor.constraint(equalToConstant: 150),
              //contentInset = UIEdgeInsets(top: 10, left: 0, bottom: 10, right: 0)
 
@@ -387,7 +492,8 @@ class TimeEntry:UIViewController,UIPickerViewDelegate, UIPickerViewDataSource{
             // lblMyDay.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             lblMyDay.topAnchor.constraint(equalTo: pickerView.bottomAnchor,constant: 10),
             lblMyDay.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor,constant:2),
-            lblMyDay.widthAnchor.constraint(equalTo: view.layoutMarginsGuide.widthAnchor, multiplier: 1, constant: -2)
+            lblMyDay.widthAnchor.constraint(equalTo: view.layoutMarginsGuide.widthAnchor, multiplier: 1, constant: -2),
+            
         ])
         
         
